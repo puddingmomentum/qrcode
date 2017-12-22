bbff855f69efbde677d86b180de1f726b7291cf0
Rob O'Dwyer <robert.martin.odwyer@gmail.com>
2017-12-22 19:22:55 +0000

    initial qr commit

---
+++ qrcode/src/main/java/com/horan/qr/App.java
@@ -0,0 +1,13 @@
+package com.horan.qr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
